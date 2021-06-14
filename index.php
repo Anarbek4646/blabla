8e0f59ccc873e0a6d40479cba381f810ad8e3110
Anarbek4646 <londonisthecapita66@gmail.com>
2021-06-14 15:23:03 +0600

    dg

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mySite</title>
-    <link rel="stylesheet" href="css/index.css">
+    
     <link rel="icon" href="fiveicon.ico">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -14,7 +14,152 @@
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Montserrat&family=Open+Sans&family=Raleway&display=swap" rel="stylesheet">
 </head>
 <body>
+<style>
+     body {
+    background-color: rgb(209, 245, 245);
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+}
+.container2 {
+    background-color: rgb(184, 226, 243);
+    overflow: auto; /* Добавляем полосы прокрутки */
+    padding-left: 15%; /* Отступ от текста слева */
+    position: fixed;
+    z-index: 101;
+    margin-top: -1.2%;
+    width: 100%;
+    height: 234px;
     
+}
+ .container23 {
+    font-family: 'Dancing Script', cursive;
+    color: skyblue;
+    
+     
+    width: 100%;
+ }
+
+ .container25 {
+     color: skyblue;
+    padding-left: 8%;
+    font-family: 'Dancing Script', cursive;
+     
+ }
+ .container24 {
+     color: skyblue;
+    font-family: 'Montserrat', sans-serif;
+ }
+
+.empaty {
+    color: rgb(209, 245, 245);
+}
+.position2 {
+    font-family: 'Dancing Script', cursive;
+    font-size: 50px;
+    color: skyblue;
+}
+ .position{ 
+    color: rgb(10, 10, 10);
+    font-size: 40px;
+    width: 900px;
+    font-family: 'Montserrat', sans-serif;
+ }
+ .position4 {
+    font-family: 'Dancing Script', cursive;
+    font-size: 50px;
+    color: skyblue;
+    margin-left: 17%;
+    position: left;
+    float: left;
+    margin-top: 15%;
+ }
+ .position3 {
+    color: rgb(10, 10, 10);
+    font-size: 40px;
+    width: 600px;
+    font-family: 'Montserrat', sans-serif;
+    margin-left: -37%;
+    float: left;
+    margin-top: 20%;
+    
+ }
+.container {
+   
+    font-family: 'Arial'; /* Гарнитура шрифта */ 
+    font-size: 3rem; /* Размер текста */ 
+   
+    position: absolute;
+    margin-left: 35%;
+    margin-top: 15rem;
+    white-space: nowrap;
+}
+
+.container3 {
+    margin-left: 35%;
+    margin-top: 50rem;
+    white-space: nowrap;
+}
+
+
+.gif {
+    position: fixed;
+    margin-top: 70rem;
+    margin-left:  43%;
+    position: absolute;
+}
+
+
+
+.profile {
+    margin-left: 50%;
+}
+.profile2 {
+    float: left;
+    margin-right: 30px;
+}
+
+.skills {
+   margin-left: 7%;
+}
+
+.socialimg {
+   
+    margin-left: 30px;
+}
+.socialimg2 {
+    margin-top: 15px;
+    margin-left: -13%;
+}
+
+
+.skills2 {
+    margin-left: 5%;
+    margin-top: 17%;
+}
+
+.skill-row {
+    margin-left: 7%;
+    
+   
+}
+
+.skill-row2 {
+    margin-right: -25%;
+    margin-top: 20%;
+    
+}
+.contact-me {
+    margin-left: 65%;
+    margin-top: -20%;
+    
+}
+.position7 {
+    color: rgb(10, 10, 10);
+    font-size: 40px;
+    width: 200px;
+    font-family: 'Montserrat', sans-serif;
+}
+    </style>
     <div class="container2">
         <img src="1729_oooo.plus.png" class="socialimg2" width="200" height="200" alt="ava">
          <a href="https://www.instagram.com/sum.qius/?hl=ru"><img src="instagram.png" class="socialimg" width="130" height="130" alt="instagram"></a>
