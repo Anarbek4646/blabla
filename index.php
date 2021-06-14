36ddb655df9892dd2c8770098b127f2c35b146b3
Anarbek4646 <londonisthecapita66@gmail.com>
2021-06-14 15:15:27 +0600

    tcfr x5vd5c f6

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mySite</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="icon" href="fiveicon.ico">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Kristi&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Montserrat&family=Open+Sans&family=Raleway&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+    <div class="container2">
+        <img src="1729_oooo.plus.png" class="socialimg2" width="200" height="200" alt="ava">
+         <a href="https://www.instagram.com/sum.qius/?hl=ru"><img src="instagram.png" class="socialimg" width="130" height="130" alt="instagram"></a>
+         <a href="https://vk.com/"><img src="vk.png" class="socialimg width="130" height="130" alt="vk"></a>
+         <a href="https://www.facebook.com/"><img src="facebook.png" class="socialimg width="130" height="130" alt="facebook"></a>
+         
+     </div>
+     <div>
+        <h1 class="empaty">hello</h1>
+     </div>
+
+   
+    <div class="container">
+        <h1 class="container23">C'v Anarbek  Alim'h</h1>
+        <p class="container25">branch of WEB-programming</p>
+    </div>
+
+    <div class="container3">
+         <img src="mountain.png" width="650" height="500" alt="ava">
+    </div>
+    
+    <div>
+    <a class="gif"><img src=Paw.gif></a>
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+
+ <!--its for my information-->
+        <div class="middle-container">
+            <div class="profile">
+              <img class="profile2" src="clipboard.png" width="200px" alt="something">
+              
+              <h2 class="position2">Hello.</h2>
+              <p class="position">this is my personal site for where you can see all my projects</p>
+              
+            </div>
+          </div>
+            <br>
+            <br>
+            <br>
+            <br>
+            
+              <div class="skill-row">
+                <img class="skills2" src="web-programming.png" width="160px" alt="script">
+                
+                <h3 class="position4">Lorem & Ipsum</h3>
+                <p class="position3">Lorem ipsum dolor sit amet, quis in duis, iaculis id felis.</p>
+                 
+              </div>
+             <div class="anarbek2">
+              <div class="skill-row2">
+                <img class="" src="" alt="">
+                <h3 class="position2">Lorem Ipsum Dolor</h3>
+                <p class="position">Lorem ipsum dolor sit amet, magna quis, iaculis id felis.</p>
+              </div>
+            
+            
+            <div class="contact-me">
+              
+              <h3 class="position2">Lorem ipsum dolor sit amet, non elit.</h3>
+              <p class="position7">Lorem ipsum dolor sit amet, in quis, aenean amet. Phasellus sodales, tellus donec dui, ornare erat.</p>
+              
+            </div>
+          
+          </div>
+          
+       
+          
+
+</body>
+</html>
